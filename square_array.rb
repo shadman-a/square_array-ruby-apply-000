6ce3ecc5db586a944b8b7733c8e63c78f3758e49
Shadman Ahmed <shadman@outlook.com>
2020-04-13 22:07:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,6 @@
 array = [1,2,3,4]
 def square_array(array)
   new_array = []
-  array.each do |num|
-    new_array << (num ** 2)
-end
+  array.each { |element|new_array << element ** 2 } 
 return new_array
 end
